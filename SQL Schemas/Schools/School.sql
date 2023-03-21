f1ee7db7aad0e46305e20196ebd8ac0e23e0ac40
naveentestsigma <naveenkumar.chandrasekar@testsigma.com>
2023-03-21 14:47:33 +0530

    space change

--- SQL Schemas/Schools/School.sql
+++ SQL Schemas/Schools/School.sql
@@ -8,7 +8,7 @@ create table Students(
 
 create table Teachers(
                          id 				bigint(10) NOT NULL auto_increment,
-                         Name 			varchar(20) default null,
+                         Name 			varchar(20)  default null,
                          Subject 		varchar(10) default null,
                          Class_room 		int			default null,
                          primary key(id)
